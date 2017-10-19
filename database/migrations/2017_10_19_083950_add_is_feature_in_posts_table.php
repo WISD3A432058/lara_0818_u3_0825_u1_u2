ab97484ce71c409c1fcf0cffe56caacd1b67cedd
M511-16\admin <s3A432058@student.nuct.edu.tw>
2017-10-19 16:50:16 +0800

    陳欣妤-運用 change(), after()等指令來調整資料表結構

--- database/migrations/2017_10_19_083950_add_is_feature_in_posts_table.php
+++ database/migrations/2017_10_19_083950_add_is_feature_in_posts_table.php
@@ -15,6 +15,7 @@ class AddIsFeatureInPostsTable extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             //
+
         });
     }
 
@@ -27,6 +28,8 @@ class AddIsFeatureInPostsTable extends Migration
     {
         Schema::table('posts', function (Blueprint $table) {
             //
+            $table->string('title',500)->change();
+            $table->boolean('is_feature')->default(false)->after('content');
         });
     }
 }
