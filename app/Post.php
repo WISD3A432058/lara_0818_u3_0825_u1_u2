71c9d48d463b38caa0dcbaee2e114c2391dbfdac
s3A432058 <s3A432058@student.ncut.edu.tw>
2017-11-12 22:39:06 +0800

    設定 Post.php的 fillable屬性

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //設定 model 對應到的資料表
     protected $table= 'posts' ;
+    
+    //設定 Post.php的 fillable屬性
+    protected $fillable= [
+        'title',
+        'content',
+        'is_feature'
+    ];
+
 }
