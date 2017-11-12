aa6c7b3cf5285b41cdbe38e23e90793717fd67d3
s3A432058 <s3A432058@student.ncut.edu.tw>
2017-11-12 23:46:44 +0800

    增加關聯

--- app/Comment.php
+++ app/Comment.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Post;
+
+
 class Comment extends Model
 {
-    //
+    //增加關聯
+    public function post()
+    {
+    return $this->belongsTo(Post::class);
+    }
 }
