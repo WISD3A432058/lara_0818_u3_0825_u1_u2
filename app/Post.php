863c1d0142f0e83ea85b4eea008a054408ac7a79
s3A432058 <s3A432058@student.ncut.edu.tw>
2017-11-12 23:45:19 +0800

    增加關聯

--- app/Post.php
+++ app/Post.php
@@ -4,11 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Comment;
+
 class Post extends Model
 {
     //設定 model 對應到的資料表
     protected $table= 'posts' ;
-    
+
     //設定 Post.php的 fillable屬性
     protected $fillable= [
         'title',
@@ -16,4 +18,10 @@ class Post extends Model
         'is_feature'
     ];
 
+    //增加關聯
+    public function comments ()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
+
