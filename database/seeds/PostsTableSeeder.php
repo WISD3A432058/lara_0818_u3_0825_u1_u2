30d431b52d16d4c77c19c9c77dba01d212409d08
s3A432058 <s3A432058@student.ncut.edu.tw>
2017-11-27 11:33:52 +0800

    在新建立的seeder檔案裡，撰寫 run方法的內容

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Post;
 
 class PostsTableSeeder extends Seeder
 {
@@ -11,6 +12,12 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // 在新建立的   seeder   檔案裡，撰寫    run     方法的內容
+       foreach (range(1,20) as $number){
+           Post::create([
+               'title'=>'title'.$number,
+               'content'=>'content'.$number,
+           ]);
+       }
     }
 }
