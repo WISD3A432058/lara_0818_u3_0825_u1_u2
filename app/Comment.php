0e6beab6581291f5b54385d04466265c9fb18579
s3A432058 <s3A432058@student.ncut.edu.tw>
2017-11-27 11:58:42 +0800

    修改Comment.php新增關聯

--- app/Comment.php
+++ app/Comment.php
@@ -9,6 +9,15 @@ use App\Post;
 
 class Comment extends Model
 {
+    //設定 model 對應到的資料表
+    protected $table= 'comments' ;
+
+    //設定 Comment.php的 fillable屬性
+    protected $fillable= [
+        'title',
+        'content_2',
+        'is_feature'
+    ];
     //增加關聯
     public function post()
     {
