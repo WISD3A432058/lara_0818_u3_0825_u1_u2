24bf6cc98db4aefafe0dbf195e4f51c33270f370
s3A432058 <s3A432058@student.ncut.edu.tw>
2017-11-27 11:36:24 +0800

    將seeder設定至DatabaseSeeder內(原先已設定好,因此多加一個註解)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(CommentsTableSeeder::class);
+        //將   seeder   設定至    DatabaseSeeder     內
         $this->call(PostsTableSeeder::class);
     }
 }
