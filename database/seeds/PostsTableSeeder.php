4c63d4961c05d0ddc0f20926af1af4bab50f9b51
s3A432058 <s3A432058@student.ncut.edu.tw>
2017-11-27 11:47:38 +0800

    使用 Faker 讓資料更擬真

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Post;
 use Carbon\Carbon;
+use Faker\Factory as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -15,17 +16,25 @@ class PostsTableSeeder extends Seeder
     {
         // 每次   seeding   前先清空資料表
         Post::truncate();
+        //使用 Faker 讓資料更擬真
+        $faker=Faker::create('zh_TW');
 
         // 在新建立的   seeder   檔案裡，撰寫    run     方法的內容
        foreach (range(1,20) as $number){
            $total= 20;
            Post::create([
+               /*
                'title'=>'title'.$number,
                'content'=>'content'.$number,
                'is_feature' => rand(0,1),
                'created_at' => Carbon::now() ->subDays($total - $number),
                'updated_at' => Carbon::now() ->subDays($total - $number),
-           ]);
+               */
+               //使用 Faker 讓資料更擬真
+               'title' => $faker->sentence,
+               'content' => $faker->paragraph,
+
+               ]);
        }
     }
 }
